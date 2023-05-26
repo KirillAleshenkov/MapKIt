9b470bf559bdbafd72930d724464a5113a1c8c28
KirillAleshenkov <125723083+KirillAleshenkov@users.noreply.github.com>
2023-05-26 22:44:59 +0500

    MapKit/3

--- app/src/main/java/com/example/mapkit/MainActivity.java
+++ app/src/main/java/com/example/mapkit/MainActivity.java
@@ -2,28 +2,20 @@ package com.example.mapkit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.graphics.PointF;
 import android.os.Bundle;
 
 import com.yandex.mapkit.Animation;
-import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
 import com.yandex.mapkit.map.CameraPosition;
-import com.yandex.mapkit.map.CompositeIcon;
-import com.yandex.mapkit.map.IconStyle;
-import com.yandex.mapkit.map.RotationType;
 import com.yandex.mapkit.mapview.MapView;
-import com.yandex.mapkit.user_location.UserLocationLayer;
-import com.yandex.mapkit.user_location.UserLocationView;
-import com.yandex.runtime.image.ImageProvider;
 
 public class MainActivity extends AppCompatActivity {
     private final String MAPKIT_API_KEY = "6953bbfa-0280-46af-a53b-56ece8baa13a";
-    private final Point TARGET_LOCATION = new Point(51.12, 58.37);
+    private final Point TARGET_LOCATION = new Point(51.2049, 58.5668);
     private MapView mapView;
-    private UserLocationLayer userLocationLayer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         MapKitFactory.setApiKey(MAPKIT_API_KEY);
@@ -31,15 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
         mapView = (MapView)findViewById(R.id.mapview);
-        mapView.getMap().setRotateGesturesEnabled(false);
-        mapView.getMap().move(new CameraPosition(new Point(0, 0), 14, 0, 0));
-
-        MapKit mapKit = MapKitFactory.getInstance();
-        userLocationLayer = mapKit.createUserLocationLayer(mapView.getMapWindow());
-        userLocationLayer.setVisible(true);
-        userLocationLayer.setHeadingEnabled(true);
-
-        //userLocationLayer.setObjectListener(this);
+        mapView.getMap().move(
+                new CameraPosition(TARGET_LOCATION, 14.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 5),
+                null);
     }
     @Override
     protected void onStop() {
@@ -55,34 +42,5 @@ public class MainActivity extends AppCompatActivity {
         MapKitFactory.getInstance().onStart();
         mapView.onStart();
     }
-    public void onObjectAdded(UserLocationView userLocationView) {
-        userLocationLayer.setAnchor(
-                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.5)),
-                new PointF((float)(mapView.getWidth() * 0.5), (float)(mapView.getHeight() * 0.83)));
 
-        userLocationView.getArrow().setIcon(ImageProvider.fromResource(
-                this, R.drawable.user_arrow));
-
-        CompositeIcon pinIcon = userLocationView.getPin().useCompositeIcon();
-
-        pinIcon.setIcon(
-                "icon",
-                ImageProvider.fromResource(this, R.drawable.icon),
-                new IconStyle().setAnchor(new PointF(0f, 0f))
-                        .setRotationType(RotationType.ROTATE)
-                        .setZIndex(0f)
-                        .setScale(1f)
-        );
-
-        pinIcon.setIcon(
-                "pin",
-                ImageProvider.fromResource(this, R.drawable.search_result),
-                new IconStyle().setAnchor(new PointF(0.5f, 0.5f))
-                        .setRotationType(RotationType.ROTATE)
-                        .setZIndex(1f)
-                        .setScale(0.5f)
-        );
-
-        userLocationView.getAccuracyCircle().setFillColor(Color.BLUE & 0x99ffffff);
-    }
 }
