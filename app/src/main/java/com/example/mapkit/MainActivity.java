9077f43a3fd74ce78b7de213ba0c6e8ac818afd9
vkomlev <victor.v.komlev@gmail.com>
2023-06-05 19:00:38 +0500

    MapKit/8

--- app/src/main/java/com/example/mapkit/MainActivity.java
+++ app/src/main/java/com/example/mapkit/MainActivity.java
@@ -14,9 +14,13 @@ import android.graphics.PointF;
 import android.os.Bundle;
 
 
+import com.google.android.libraries.maps.SupportMapFragment;
+import com.google.android.libraries.maps.model.LatLng;
+import com.google.android.libraries.maps.model.PolylineOptions;
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.geometry.Polyline;
 import com.yandex.mapkit.layers.ObjectEvent;
 import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.map.CompositeIcon;
@@ -28,6 +32,9 @@ import com.yandex.mapkit.user_location.UserLocationLayer;
 import com.yandex.mapkit.user_location.UserLocationObjectListener;
 import com.yandex.mapkit.user_location.UserLocationView;
 import com.yandex.runtime.image.ImageProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements UserLocationObjectListener{
     private final String MAPKIT_API_KEY = "6953bbfa-0280-46af-a53b-56ece8baa13a";
@@ -53,6 +60,22 @@ public class MainActivity extends AppCompatActivity implements UserLocationObjec
         userLocationLayer.setHeadingEnabled(true);
         userLocationLayer.setObjectListener((UserLocationObjectListener) this);
         mapView.getMap().getMapObjects().addPlacemark(new Point(51.207970, 58.563277)).setIcon(ImageProvider.fromResource(this, R.drawable.icon));
+
+        ArrayList<LatLng> coordList = new ArrayList<LatLng>();
+
+        coordList.add(new LatLng(0, 0),
+        coordList.add(new LatLng(1, 1),
+        coordList.add(new LatLng(2, 2);
+
+
+        PolylineOptions polylineOptions = new PolylineOptions();
+
+        polylineOptions.addAll(coordList);
+        polylineOptions
+                .width(5)
+                .color(Color.RED);
+        polylineOptions.add());
+
     }
     @Override
     protected void onStop() {
